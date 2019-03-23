76344f0366de079751801ea11787101a8eb81fbc
snegard <stiande@stud.ntnu.no>
2019-03-23 16:22:00 +0100

    Continued work on 3.2. Testing out a few methods on how to solve the problem

--- kode/globalHDR.py
+++ kode/globalHDR.py
@@ -69,7 +69,15 @@ def show(im):
 
 def luminance(im):#, chan):
     #if chan == "all":
-    lum = (im[:, :, 0] + im[:, :, 1] + im[:, :, 2])
+    lum = im
+    lum[:, :, 0] = (im[:, :, 0] + im[:, :, 1] + im[:, :, 2])
+    lum[:, :, 1] = (im[:, :, 0] + im[:, :, 1] + im[:, :, 2])
+    lum[:, :, 2] = (im[:, :, 0] + im[:, :, 1] + im[:, :, 2])
+    lum[lum > 1] = 1
+    lum[lum <= 0] = 0
+    #lume = edit(lum, "sqrt")
+    #show(lume)
+    #print(lum)
     #elif chan == "r":
     #    luminance = (im[:, :, 0])
     #elif chan == "g":
@@ -84,26 +92,56 @@ def luminance(im):#, chan):
 
 
 def chromasity(im):
-    #chrom = ((im[:, :, 0] / luminance(im)) + (im[:, :, 1] / luminance(im)) + (im[:, :, 2] / luminance(im)))
-    im = luminance(im)
-    #chrom = (im[:, :, 0] / luminance(im))
-    show(im)
-    print(im)
+    #chrom = np.zeros(im)
+    #chrom = np.zeros(im.shape)
+    #chrom = im
+    print(luminance(im))
+    chrom = im / luminance(im)
+    ##chrom[:, :, 0] = (im[:, :, 0] / luminance(im))
+    ##chrom[:, :, 1] = (im[:, :, 1] / luminance(im))
+    ##chrom[:, :, 2] = (im[:, :, 2] / luminance(im))
+    #im = luminance(im)
+    #bb = (im[:, :, :] / luminance(im[:, :, :]))
 
+    #print(chrom)
+    #print(im.shape)
+    #print(im)
+    #show(im)
+    return chrom
+
+
+def split_image(im):
+    #show(im)
+    lum = im
+    chrom = im
+    lum = luminance(lum) * .75
+    chrom = chromasity(chrom)
+    #print("lum", lum.shape)
+    #print("ch", chrom.shape)
+
+    image = lum * chrom
+    show(image)
+
+    #show(im)
+    #show(lum)
+    #show(chrom)
 
 
 
 
 image = read_image()
-edited = edit(image, "sqrt")
-show(image)
+#edited = edit(image, "sqrt")
+#print(image)
+#show(image)
 #show(edited)
 #print(edited)                       # hele matrisen
 #print(edited.shape, len(edited))    # (784, 1000, 3) # len(edited) 784
 
+#luminance(image)
 #chromasity(image)
+split_image(image)
 
-# r = image[:, :, 0]            # [0] rød
+#r = image[:, :, 0]            # [0] rød
 # g = image[:, :, 1]            # [1] grønn
 # b = image[:, :, 2]            # [2] blå
 
