0889cd8ef57c9b02bee98626b78a84ac9ae66e5c
Mats Eikeland Mollestad <mem@mollestad.no>
2019-03-26 22:20:36 +0100

    Some cleanup in pipeline

--- pipelinelint.bash
+++ pipelinelint.bash
@@ -2,13 +2,11 @@ pylint kode/*.py --rcfile=lint-config.rc || true # Ignorerer exit code
 values=$(pylint kode/*.py --rcfile=lint-config.rc | grep rated | awk '{print $7}')
 IFS='/'
 score=$(echo $values | awk '{print $1}')
-echo $score
 IFS='.'
 score=$(echo $score | awk '{print $1$2}')
-echo $score
 if [ "$score" -gt "950" ]; then
     exit 0
 else
-    echo "To low code rating $((score / 10)) < 9.5"
+    echo "To low code rating $score < 950"
     exit 1
 fi
