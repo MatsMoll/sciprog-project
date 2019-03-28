892dddb8148d9b14928710b408bf38920757180b
snegard <stiande@stud.ntnu.no>
2019-03-28 20:19:10 +0100

    Progress on 4.1. Minor adjustments needed! (last commit stated wrong task number.)

--- kode/localHDR.py
+++ kode/localHDR.py
@@ -8,39 +8,51 @@ import globalHDR
 import scipy.ndimage as ndimage
 
 
-def filter_linear_spatial(im, limit=.5):
-    globalHDR.show(im)
-    
-    grey = im.astype(float).sum(2) / (255 * 3)
-    blurry = np.zeros(im.shape)
-    #blurry = ndimage.uniform_filter(grey, size=15)
-    for i in range(0, im.ndim):
-        blurry[:, :, i] = ndimage.uniform_filter(grey, size=15)
-    globalHDR.show(blurry)
-    
-                #details = np.zeros(im.shape)
-    #print("im:", im.shape, "details:", details.shape, "blurry:", blurry.shape)
-    details = im - blurry
-                #for i in range(0, details.ndim):
-                #    details[:, :, i] = im[:, :, i] - blurry
-                #    # details = im - blurry
+def filter_linear_spatial(im, mode="global"):  # limit=.5):
+    """
+    Docstring
 
-    #print(np.percentile(details, 90))
-    limit = limit * 0 + np.percentile(details, 90)  # Dette betyr at kun 10% = detaljer
-                                                    #  * 2 # * 3 # 0.5 # details.mean() # Threshold for hva som "er" detaljer
-    details[details > limit] = 1
-    details[details <= limit] = 0
-    globalHDR.show(details)
+    :param im: Input image.
+    :param mode: Editing mode. (Global | Luminance)
+    :return:
+    """
+    # Laster inn originalbildet
+    globalHDR.show(im)
+    shape = im.shape
+
+    # Lager et monokromt bilde og et blurry bilde
+    grey_im = im.astype(float).sum(2) / (255 * 3)
+    #-blurry_im = np.zeros(shape)
+    blurry_im = ndimage.uniform_filter(grey_im, size=15)
+    #for i in range(0, im.ndim):
+    #    grey_im[:, :, i] = im.astype(float).sum(2) / (255 * 3)
+    globalHDR.show(blurry_im)
+
+    #Definerer detaljene ved (original - blurry)
+    #-detail_im = np.zeros(shape)
+    #print("im:", im.shape, "detail_im:", detail_im.shape, "blurry_im:", blurry_im.shape)
+    for i in range(0, im.ndim):
+        detail_im = im[:, :, i] - blurry_im
+
+    limit = np.percentile(detail_im, 95)  # Dette betyr at kun 10% = detaljer
+                                                    #  * 2 # * 3 # 0.5 # detail_im.mean() # Threshold for hva som "er" detaljer
+    detail_im[detail_im > limit] = 1
+    detail_im[detail_im <= limit] = 0
+    globalHDR.show(detail_im)
     
-    blurry_edited = globalHDR.edit_globally(blurry)  # sqrt er default
-    # blurry_edited = globalHDR.edit_luminance(blurry)
-                ##filtered = np.zeros(details.shape)
-    filtered = blurry_edited + details
-                ##for i in range(0, details.ndim):
-                ##    filtered[:, :, i] = blurry_edited + details[:, :, i]
-    globalHDR.show(filtered)
-    
-    globalHDR.compare(details, filtered)
+    if mode == "global":
+        blurry_im_edited = globalHDR.edit_globally(blurry_im)  # sqrt er default
+    else:
+        blurry_im_edited = globalHDR.edit_luminance(blurry_im)  # sqrt er default
+
+    filtered_im = np.zeros(shape)
+    for i in range(0, detail_im.ndim):
+        filtered_im[:, :, i] = detail_im + blurry_im_edited
+    globalHDR.show(filtered_im)
+
+    print("\nim", im.shape, "\ngrey_im", grey_im.shape, "\nblurry_im", blurry_im.shape, "\ndetail_im", detail_im.shape, "\nblurry_im_edited", blurry_im_edited.shape, "\nfiltered_im", filtered_im.shape)
+    print(blurry_im_edited)
+    #globalHDR.compare(blurry_im, blurry_im_edited)
 
 
 filter_linear_spatial(globalHDR.read_image())
