4f00bb359e5f6ac4ce8cb1802e7d0c2023c164c8
snegard <stiande@stud.ntnu.no>
2019-03-29 11:33:28 +0100

    minor adjustments

--- kode/localHDR.py
+++ kode/localHDR.py
@@ -22,19 +22,19 @@ def filter_linear_spatial(im, mode="global"):  # limit=.5):
 
     # Lager et monokromt bilde og et blurry bilde
     grey_im = im.astype(float).sum(2) / (255 * 3)
-    #-blurry_im = np.zeros(shape)
+        #-blurry_im = np.zeros(shape)
     blurry_im = ndimage.uniform_filter(grey_im, size=15)
-    #for i in range(0, im.ndim):
-    #    grey_im[:, :, i] = im.astype(float).sum(2) / (255 * 3)
+        #for i in range(0, im.ndim):
+        #    grey_im[:, :, i] = im.astype(float).sum(2) / (255 * 3)
     globalHDR.show(blurry_im)
 
     #Definerer detaljene ved (original - blurry)
-    #-detail_im = np.zeros(shape)
-    #print("im:", im.shape, "detail_im:", detail_im.shape, "blurry_im:", blurry_im.shape)
+    detail_im = np.zeros(shape)
+        #print("im:", im.shape, "detail_im:", detail_im.shape, "blurry_im:", blurry_im.shape)
     for i in range(0, im.ndim):
-        detail_im = im[:, :, i] - blurry_im
+        detail_im[:, :, i] = im[:, :, i] - blurry_im
 
-    limit = np.percentile(detail_im, 95)  # Dette betyr at kun 10% = detaljer
+    limit = np.percentile(detail_im, 95)  # Dette betyr at kun 5% = detaljer
                                                     #  * 2 # * 3 # 0.5 # detail_im.mean() # Threshold for hva som "er" detaljer
     detail_im[detail_im > limit] = 1
     detail_im[detail_im <= limit] = 0
@@ -47,12 +47,22 @@ def filter_linear_spatial(im, mode="global"):  # limit=.5):
 
     filtered_im = np.zeros(shape)
     for i in range(0, detail_im.ndim):
-        filtered_im[:, :, i] = detail_im + blurry_im_edited
+        filtered_im[:, :, i] = detail_im[:, :, i] + blurry_im_edited
     globalHDR.show(filtered_im)
 
     print("\nim", im.shape, "\ngrey_im", grey_im.shape, "\nblurry_im", blurry_im.shape, "\ndetail_im", detail_im.shape, "\nblurry_im_edited", blurry_im_edited.shape, "\nfiltered_im", filtered_im.shape)
-    print(blurry_im_edited)
+    #print(detail_im)
     #globalHDR.compare(blurry_im, blurry_im_edited)
 
+    alpha = 30
+    sharp_im = np.zeros(shape)
+    for i in range(0, im.ndim):
+        #sharp_im[:, :, i] = blurry_im + alpha * (blurry_im - blurry_im_edited)
+        sharp_im[:, :, i] = detail_im[:, :, i] + alpha * (blurry_im - blurry_im_edited)
+    sharp_im = np.abs(sharp_im)
+    print(sharp_im)
+    globalHDR.show(sharp_im)
 
+
+# sharpened = blurred_f + alpha * (blurred_f - filter_blurred_f)
 filter_linear_spatial(globalHDR.read_image())
