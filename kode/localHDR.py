558b92fcc71d09912c64f6b4ad08fb420be3893d
snegard <stiande@stud.ntnu.no>
2019-03-31 12:22:01 +0200

    4.1: Finished the task, documented and structured the code.

--- kode/localHDR.py
+++ kode/localHDR.py
@@ -1,5 +1,5 @@
 """
-...
+This module is rendering the input image with a local function.
 
 """
 
@@ -10,79 +10,96 @@ import scipy.ndimage as ndimage
 
 def blur_image(im, sigma=3):
     """
+    Blurs an image with the gaussian filter with a set range.
 
-    :param im:
-    :param sigma:
-    :return:
+    :param im: Input image.
+    :param sigma: Range of gaussian filter.
+    :return: Blurred image.
     """
-    blurry_im = np.zeros(im.shape)
     if im.ndim <= 2:
         blurry_im = ndimage.gaussian_filter(im, sigma)
-        # blurry_im = ndimage.uniform_filter(im, size=9)
     else:
-        for i in range(0, im.ndim):  # til im.shape[-1]. tidligere: im.ndim men den funker ikke på svart-hvitt. må ta siste skuffen til dimensjonene
+        blurry_im = np.zeros(im.shape)
+        for i in range(0, im.ndim):
             blurry_im[:, :, i] = ndimage.gaussian_filter(im[:, :, i], sigma)
-            # blurry_im[:, :, i] = ndimage.uniform_filter(im[:, :, i], size=9)
     # globalHDR.show(blurry_im)
     return blurry_im
 
 
-def find_details(im, blurry_im, level=98):
+def find_details(im, blurry_im, level):
     """
+    Extracts the details of the image.
+    The details is defined by Uh = U0 - Ul,
+        where Uh = details, U0 = original image and Ul = blurred image.
 
-    :param im:
-    :param level:
-    :return:
+    :param im: Input image.
+    :param blurry_im: Input blurred image.
+    :param level: Detail level.
+    :return: Detailed image.
     """
-    detail_im = im - blurry_im  # detail = Uh, im = U0, blurry = Ul
+    detail_im = im - blurry_im
 
-    limit = np.percentile(detail_im, level)  # Dette betyr at kun topp 2% verdier = detaljer
+    limit = np.percentile(detail_im, level)
     detail_im[detail_im > limit] = 1
     detail_im[detail_im <= limit] = 0
     # globalHDR.show(detail_im)
     return detail_im
 
 
-def edit_blurred_image(blurry_im, mode="global", lum_scale=10, chrom_scale=.3):
+def edit_blurred_image(blurry_im, mode, lum_scale, chrom_scale):
     """
+    Edits a blurred image with a given mode and scale. Reuses functions from global rendering.
+    Mode decides whether it is edited on the luminance channel or globally.
 
-    :return:
+    :param blurry_im: Input image (blurred).
+    :param mode: Editing mode. (Global | Luminance)
+    :param lum_scale: Weighting of luminance.
+    :param chrom_scale: Weighting of chromasity.
+    :return: Edited, blurred image.
     """
     if mode == "global":
-        blurry_im_edited = globalHDR.edit_globally(blurry_im)  # sqrt er default
+        blurry_im_edited = globalHDR.edit_globally(blurry_im)
     else:
-        blurry_im_edited = globalHDR.edit_luminance(blurry_im, lum_scale, chrom_scale)  # sqrt er default
+        blurry_im_edited = globalHDR.edit_luminance(blurry_im, lum_scale, chrom_scale)
     # globalHDR.show(blurry_im_edited)
     return blurry_im_edited
 
 
-def reconstruct_image(detail_im, blurry_im, alpha=1):
+def reconstruct_image(detail_im, blurry_im, alpha):  # Unsure if the weight is necessary.
     """
+    Reconstructs the image with a given weight.
 
-    :param detail_im:
-    :param blurry_im:
-    :param alpha:
-    :return:
+    :param detail_im: Detailed part of the original image.
+    :param blurry_im: Blurred part of the original image.
+    :param alpha: Weighting of details.
+    :return: Reconstructed image.
     """
     return detail_im * alpha + blurry_im
 
 
-def filter_linear_spatial(im, mode="global"):
+def filter_linear(im, sigma=3, level=90, mode="global", lum_scale=10, chrom_scale=.3, alpha=5):
     """
-    Docstring
+    Filters the blurred and detailed parts of the image,
+        edits the blurred parts and puts it back together.
+    Multiple weighting and level options are provided to edit the image the way you want.
 
     :param im: Input image.
+    :param sigma: Range of gaussian filter.
+    :param level: Detail level.
     :param mode: Editing mode. (Global | Luminance)
+    :param lum_scale: Weighting of luminance.
+    :param chrom_scale: Weighting of chromasity.
+    :param alpha: Weighting of details.
     :return:
     """
     # globalHDR.show(im)
-    blurry_im = blur_image(im, 3)
-    detail_im = find_details(im, blurry_im, 90)
-    blurry_im_edited = edit_blurred_image(blurry_im, mode, 1, 1)
-    filtered_im = reconstruct_image(detail_im, blurry_im_edited, 1)
-
+    blurry_im = blur_image(im, sigma)
+    detail_im = find_details(im, blurry_im, level)
+    blurry_im_edited = edit_blurred_image(blurry_im, mode, lum_scale, chrom_scale)
+    filtered_im = reconstruct_image(detail_im, blurry_im_edited, alpha)
     return filtered_im
 
 
-filtered = filter_linear_spatial(globalHDR.read_image("../eksempelbilder/Ocean/Ocean"))
-globalHDR.show(filtered)
+input = globalHDR.read_image("../eksempelbilder/Balls/Balls")
+filtered_im = filter_linear(input, 3, 98, "global", 10, .3, 1)
+globalHDR.show(filtered_im)
