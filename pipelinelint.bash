7ac906622b7e44070f80a50412eed272b31b7e02
Mats Eikeland Mollestad <mem@mollestad.no>
2019-04-24 11:11:56 +0200

    Minor changes to pipeline

--- pipelinelint.bash
+++ pipelinelint.bash
@@ -11,13 +11,13 @@ fi
 
 cd kode
 coverage run align_image_test.py
-coverage run hdr_test.py
-coverage run globalHDR_test.py
-coverage run localHDR_test.py
-coverage run gradient_compression_test.py
-coverage report
+coverage run -a hdr_test.py
+coverage run -a globalHDR_test.py
+coverage run -a localHDR_test.py
+coverage run -a gradient_compression_test.py
 pros=$(coverage report | grep TOTAL | awk '{print $4}')
 pros=${pros%\%}
+coverage report
 echo $pros
 if [ "$pros" -lt "50" ]; then
     echo "To low coverage $pros < 50"
